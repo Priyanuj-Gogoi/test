197aeda84bff1c0ac21386a9c5a0f8390c085feb
Priyanuj Gogoi <priyanuj289@gmail.com>
2022-11-07 15:26:57 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
   tagline: 'Dinosaurs are cool',
   url: 'https://priyanuj-gogoi.github.io',
   baseUrl: '/docusaurus-test/',
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   trailingSlash: false,
@@ -40,13 +40,6 @@ const config = {
           editUrl:
             'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
@@ -61,7 +54,7 @@ const config = {
         title: 'BDFD Templates',
         logo: {
           alt: 'bdfdLogo',
-          src: 'image/bdfd.png',
+          src: 'img/bdfd.png',
         },
         items: [
           {
@@ -70,8 +63,6 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
-          {to: '/changelog', label: 'Changelog', position: 'left'},
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
             label: 'GitHub',
