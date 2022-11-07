4375e5ad39059c2516cb02b80f7b0f6535b35a8b
Priyanuj Gogoi <priyanuj289@gmail.com>
2022-11-07 15:05:29 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -70,6 +70,7 @@ const config = {
             position: 'left',
             label: 'Tutorial',
           },
+          {to: '/changelog', label: 'Changelog', position: 'left'},
           {to: '/blog', label: 'Blog', position: 'left'},
           {
             href: 'https://github.com/facebook/docusaurus',
