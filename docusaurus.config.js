4337e3a4166e243187076cb82ff5269abc32213f
Priyanuj Gogoi <priyanuj289@gmail.com>
2022-11-07 13:31:34 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://priyanuj-gogoi.github.io',
-  baseUrl: '/',
+  baseUrl: '/docusaurus-test/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
