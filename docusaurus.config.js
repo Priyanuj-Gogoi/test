a079eb40bbb4d6347b35c1ce3e226e9a2ce5fdc5
Priyanuj Gogoi <priyanuj289@gmail.com>
2022-11-08 15:43:02 +0530

    Code format

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -67,35 +67,17 @@ const config = {
           {
             title: 'Community',
             items: [
-              {
-                label: 'Discord',
-                href: 'https://discord.com/servers/bot-designer-for-discord-official-server-566363823137882154',
-              },
-              {
-                label: 'Reddit',
-                href: 'https://www.reddit.com/r/DiscordBotDesigner/'
-              },
+              { label: 'Discord', href: 'https://discord.com/servers/bot-designer-for-discord-official-server-566363823137882154' },
+              { label: 'Reddit', href: 'https://www.reddit.com/r/DiscordBotDesigner/' },
             ],
           },
           {
             title: 'External Links',
             items: [
-              {
-                label: 'Website',
-                href: 'https://botdesignerdiscord.com'
-              },
-              {
-                label: 'Android App',
-                href: 'https://play.google.com/store/apps/details?id=com.jakubtomana.discordbotdesinger',
-              },
-              {
-                label: 'iOS App',
-                href: 'https://apps.apple.com/app/bot-designer-for-discord/id1495536477',
-              },
-              {
-                label: 'Web App',
-                href: 'https://botdesignerdiscord.com/app',
-              },
+              { label: 'Website', href: 'https://botdesignerdiscord.com' },
+              { label: 'Android App', href: 'https://play.google.com/store/apps/details?id=com.jakubtomana.discordbotdesinger' },
+              { label: 'iOS App', href: 'https://apps.apple.com/app/bot-designer-for-discord/id1495536477' },
+              { label: 'Web App', href: 'https://botdesignerdiscord.com/app' },
             ],
           },
         ],
