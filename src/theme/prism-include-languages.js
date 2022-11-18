c0644e124ee72a5b331ebf257e8352ab625d1833
প্রিয়ানুজ গগৈ <priyanuj289@gmail.com>
2022-11-18 15:30:02 +0530

    Update prism-include-languages.js

--- src/theme/prism-include-languages.js
+++ src/theme/prism-include-languages.js
@@ -22,27 +22,27 @@ export default function prismIncludeLanguages(Prism) {
          'pattern': /(?<Arithmetic>[+\-*\/%])|(?<Relational>[=!]=|[<>]=?)/,
          'greedy': true
      },
-     'escape-characters': [
+     'bds-escape-characters': [
            { 'pattern': /\\[\\\]\[;]/, 'alias': 'entity' },
            { 'pattern': /%(ESCAPED|\{-?(D|SEMIC)OL-?\})%/, 'alias': 'entity' }
       ],
-     'callbacks': {
-         'pattern': /^\$a(?:waitedCom{2}and(?:Er{2}or)?|lwaysReply)|on(?:AutoComplete|Joined|Leave|Interaction|Mes{2}ageDelete|Ban(?:Ad{2}|Remove))|reaction|mes{2}ageContains/,
+     'bds-callbacks': {
+         'pattern': /\$a(?:waitedCom{2}and(?:Er{2}or)?|lwaysReply)|on(?:AutoComplete|Joined|Leave|Interaction|Mes{2}ageDelete|Ban(?:Ad{2}|Remove))|reaction|mes{2}ageContains/,
          'greedy': true
       },
-     'variable': {
+     'bds-variables': {
           'pattern': /\$(?:[gs]et(?:Chan{2}el|(?:Serv|Us)er)?V|v)ar/,
           'greedy': true
       },
-     'entity': {
+     'bds-http-funcs': {
          'pattern': /(?:\$)ht{2}p(?:(?:Get(?:Header)?|Ad{2}Header)|P(?:ost|ut|atch)|Delete|Result|Status)/,
          'greedy': true
      },
-     'keyword': {
+     'bds-funcs': {
           'pattern': /\$(addButton|addCmdReactions|addEmoji|addField|addReactions|addSelectMenuOption|addTextInput|addTimestamp|allMembersCount|allowMention|allowRoleMentions|allowUserMentions|alternativeParsing|and|appendOptionSuggestion|argCount|argsCheck|async|authorAvatar|authorID|authorIcon|authorOfMessage|authorURL|author|autoCompleteOptionName|autoCompleteOptionValue|awaitFunc|awaitReactions|await|ban|banID|blackListIDs|blackListRolesIDs|blackListRoles|blackListServers|blackListUsers|boostCount|botCommands|botID|botLeave|botListDescription|botListHide|botNode|botOwnerID|botTyping|calculate|catch|categoryCount|categoryID|changeCooldownTime|changeUsernameWithID|changeUsername|channelCount|channelExists|channelID|channelIDFromName|channelName|channelPosition|channelSendMessage|channelTopic|channelType|charCount|checkCondition|checkContains|checkUserPerms|clear|clearReactions|clear|closeTicket|colorRole|color|commandsCount|cooldown|createChannel|createRole|creationDate|cropText|customEmoji|customID|customImage|date|day|defer|deleteChannelsByName|deleteChannels|deleteIn|deleteRole|deletecommand|description|disableSpecialEscaping|discriminator|divide|dm|dmChannelID|editButton|editChannelPerms|editEmbedIn|editIn|editMessage|editSelectMenuOption|editSelectMenu|editThread|elseif|else|embedSuppressErrors|embeddedURL|emoteCount|enableDecimals|enabled|endasync|endif|endtry|ephemeral|error|eval|executionTime|findChannel|findRole|findUser|footerIcon|footer|getBotInvite|getCooldown|getCustomStatus|getEmbedData|getInviteInfo|getLeaderboardValue|getMessage|getReactions|getRoleColor|getServerInvite|getTextSplitIndex|getTextSplitLength|getTimestamp|getUserStatus|giveRole|globalCooldown|globalUserLeaderboard|guildExists|guildID|hasRole|highestRole|highestRoleWithPerms|highestRole|hostingExpireTime|hour|httpAddHeader|httpDelete|httpGetHeader|httpGet|httpPatch|httpPost|httpPut|httpRemoveHeader|httpResult|httpStatus|hypesquad|if|ignoreChannels|ignoreLinks|ignoreTriggerCase|image|input|isAdmin|isBanned|isBoolean|isBot|isHoisted|isMentionable|isNSFW|isNumber|isSlash|isTimedOut|isUserDMEnabled|isValidHex|joinSplitText|kick|kickMention|lowestRole|lowestRoleWithPerms|lowestRole|max|membersCount|membersCount|mentionedChannels|mentionedRoles|mentioned|message|messageID|min|minute|modifyChannelPerms|modifyChannel|modifyRolePerms|modifyRole|modulo|month|multi|mute|newModal|newSelectMenu|newTicket|nickname|noMentionMessage|nomention|numberSeparator|onlyAdmin|onlyBotChannelPerms|onlyBotPerms|onlyForCategories|onlyForChannels|onlyForIDs|onlyForRoleIDs|onlyForRoles|onlyForServers|onlyForUsers|onlyIfMessageContains|onlyIf|onlyNSFW|onlyPerms|optOff|or|pinMessage|ping|premiumExpireTime|publishMessage|random|randomChannelID|randomMention|randomString|randomText|randomUser|randomUserID|random|registerGuildCommands|removeButtons|removeComponent|removeContains|removeLinks|removeSplitTextElement|repeatMessage|replaceText|repliedMessageID|reply|replyIn|resetServerVar|resetUserVar|roleCount|roleExists|roleGrant|roleID|roleInfo|roleName|roleNames|rolePosition|round|scriptLanguage|second|sendEmbedMessage|sendMessage|sendNotification|serverChannelExists|serverCooldown|serverCount|serverDescription|serverIcon|serverInfo|serverLeaderboard|serverName|serverNames|serverOwner|serverRegion|serverVerificationLvl|shardID|slashCommandsCount|slashID|slowmode|sort|splitText|startThread|stop|sub|sum|suppressErrors|takeRole|textSplit|threadAddMember|threadRemoveMember|thumbnail|time|timeout|title|toLowercase|toTitleCase|toUppercase|trimContent|trimSpace|try|tts|unban|unbanID|unescape|unmute|unpinMessage|unregisterGuildCommands|untimeout|uptime|url|useChannel|usedEmoji|userAvatar|userExists|userID|userInfo|userJoinedDiscord|userJoined|userLeaderboard|userPerms|userReacted|userRoles|userServerAvatar|username|varExistError|varExists|variablesCount|webhookAvatarURL|webhookColor|webhookContent|webhookCreate|webhookDelete|webhookDescription|webhookFooter|webhookSend|webhookTitle|webhookUsername|year)/
        },
-     'comment': /\$c\[[\s\S]*?\]/,
-     'symbol': /[\[\;\]]/
+     'bds-comment': /\$c\[[\s\S]*?\]/,
+     'bds-symbol': /[\[\;\]]/
    };
 
    Prism.languages.bds = Prism.languages.bdscript;
