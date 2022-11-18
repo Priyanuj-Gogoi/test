a5b337c83a59df971704915a035d51b438e0ab7c
প্রিয়ানুজ গগৈ <priyanuj289@gmail.com>
2022-11-18 17:56:21 +0530

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -95,54 +95,24 @@ module.exports = {
   plain: { color: '#f8f8f2', backgroundColor: '#272822' },
   styles: [
     // Okadia Theme
-    { types: ['changed'], style: { color: 'rgb(162, 191, 252)', fontStyle: 'italic' }},
-    { types: ['deleted'], style: { color: '#f92672' }},
-    { types: ['inserted'], style: { color: 'rgb(173, 219, 103)' }},
-    { types: ['comment'], style: { color: '#8292a2', fontStyle: 'italic' }},
+    { types: ['changed'], style: { color: 'rgb(162, 191, 252)', fontStyle: 'italic' } },
+    { types: ['deleted'], style: { color: '#f92672' } },
+    { types: ['inserted'], style: { color: 'rgb(173, 219, 103)' } },
+    { types: ['comment'], style: { color: '#8292a2', fontStyle: 'italic' } },
     { types: ['symbol'], style: { color: '#F7639C' }},
-    { types: ['keyword'], style: { color: '#8553FF' }},
-    { types: ['callbacks'], style: { color: '#32A852' }},
-    { types: ['string', 'url'], style: { color: '#a6e22e' }},
-    { types: ['variable'], style: { color: '#f8f8f2' }},
-    { types: ['number'], style: { color: '#ae81ff' }},
-    { types: ['builtin', 'char', 'constant', 'function'], style: { color: '#e6db74' }},
-    { types: ['punctuation'], style: { color: '#f8f8f2' }},
-    { types: ['selector', 'doctype'], style: { color: '#a6e22e', fontStyle: 'italic' }},
-    {
-      types: ["class-name"],
-      style: {
-        color: "rgb(255, 203, 139)",
-      },
-    },
-    {
-      types: ["tag"],
-      style: {
-        color: "rgb(127, 219, 202)",
-      },
-    },
-    {
-      types: ["boolean"],
-      style: {
-        color: "rgb(255, 88, 116)",
-      },
-    },
-    {
-      types: ["property"],
-      style: { color: "rgb(128, 203, 196)" }
-    },
-    {
-      types: ["namespace"],
-      style: { color: "rgb(178, 204, 214)" }
-    },
-    {
-      types: ['entity'],
-      style: { color: "#FF6E6E" }
-    },
-    { types: ['url'], style: { color: '#8fbcff', fontStyle: 'italic' }},
-    {
-       types: ['operator'],
-       style: { color: 'rgb(200,162,200)' }
-    },
+    { types: ['operator', 'tag', 'keyword'], style: { color: '#66d9ef' } },
+    { types: ['string', 'url'], style: { color: '#a6e22e' } },
+    { types: ['variable'], style: { color: '#f8f8f2' } },
+    { types: ['number'], style: { color: '#ae81ff' } },
+    { types: ['builtin', 'char', 'constant', 'function', 'class-name'], style: { color: '#e6db74' } },
+    { types: ['punctuation'], style: { color: '#f8f8f2' } },
+    { types: ['selector', 'doctype'], style: { color: '#a6e22e', fontStyle: 'italic' } },
+    { types: ['boolean'], style: { color: '#ae81ff' } },
+    { types: ['property'], style: { color: '#f92672' } },
+    { types: ['namespace'], style: { color: 'rgb(178, 204, 214)' } },
+    { types: ['attr-name'], style: { color: '#a6e22e !important' } },
+    { types: ['rule'], style: { color: '#e6db74' } },
+    { types: ['doctype'], style: { color: '#8292a2' } },
   ],
 }
       }
