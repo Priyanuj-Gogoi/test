fbf630f49e97491756de564406eac6ce78e332f9
প্রিয়ানুজ গগৈ <priyanuj289@gmail.com>
2022-11-19 16:36:39 +0530

    Update prism-include-languages.js

--- src/theme/prism-include-languages.js
+++ src/theme/prism-include-languages.js
@@ -3,10 +3,11 @@ import siteConfig from '@generated/docusaurus.config';
 export default function prismIncludeLanguages(Prism) {
   const { themeConfig: {prism} } = siteConfig;
   const {additionalLanguages} = prism;
+  const bdfd = require('./bdscript').definition;
 
   additionalLanguages.forEach((lang) => {
     require(`prismjs/components/prism-${lang}`);
   });
 
-  Prism.languages.bds = require('./bdscript').definition;
+  Prism.languages.bds = bdfd;
 }
